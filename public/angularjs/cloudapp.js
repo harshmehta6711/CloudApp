84fa384893785ca70f60a2c080ea86c11965bf46
harshmehta6711 <harsh.mehta@sjsu.edu>
2017-04-14 15:31:59 -0700

    Server side code for file uploads

--- public/angularjs/cloudapp.js
+++ public/angularjs/cloudapp.js
@@ -1,6 +1,12 @@
 var cloudapp=angular.module("CloudApp",[]);
 
 cloudapp.controller('cloudCtrl',function ($scope,$http) {
+$scope.uploadFile=function () {
+   $http({
+       method: 'POST',
+       url: '/upload',
 
+   })
+}
     
 });
