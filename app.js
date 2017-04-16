8b0f46f4dcbd286b564034e0d4b8cb2e841fb65f
harshmehta6711 <harsh.mehta@sjsu.edu>
2017-04-16 09:33:17 -0700

    Server side code for file uploads

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+var formidable = require('formidable'),
+    form = new formidable.IncomingForm();
+
 var app = express();
 
 // all environments
@@ -29,6 +32,33 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+// app.post('/', function (req, res) {
+//   //var form = new formidable.IncomingForm();
+//
+//   form.parse(req);
+//
+//   form.on('fileBegin', function (name, file) {
+//     file.path = __dirname + '/uploads/' + file.name;
+//   });
+//   form.on('file', function (name, file){
+//     console.log('Uploaded ' + file.name);
+//   });
+//
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// app.post('/upload', function(req, res, next) {
+//   form.parse(req, function(err, fields, files) {
+//     console.log("File received:\nName:"+files.pdf.name+"\ntype:"+files.pdf.type);
+//   });
+
+// app.post('/upload',function (req,res) {
+//   console.log("Hi Harsh"+req.data);
+//
+// });
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- public/angularjs/cloudapp.js
+++ public/angularjs/cloudapp.js
@@ -1,12 +1,17 @@
 var cloudapp=angular.module("CloudApp",[]);
 
 cloudapp.controller('cloudCtrl',function ($scope,$http) {
-$scope.uploadFile=function () {
-   $http({
-       method: 'POST',
-       url: '/upload',
+$scope.uploadFile=function (files) {
+    var fd = new FormData();
+    //Take the first selected file
+    fd.append("file", files[0]);
+    console.log(fd.filename);
+    $http.post('/upload', fd, {
+        withCredentials: true,
+        headers: {'Content-Type': undefined },
+        transformRequest: angular.identity
+    }).success(console.log('success') ).error( console.log('error') );
 
-   })
 }
     
 });
