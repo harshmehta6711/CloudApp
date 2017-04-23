684fb64df944625346a1e3bff2f53961e3dbec03
harshmehta6711 <harsh.mehta@sjsu.edu>
2017-04-23 01:20:24 -0700

    Server side code before changes

--- app.js
+++ app.js
@@ -3,14 +3,16 @@
  * Module dependencies.
  */
 
+var formidable = require('formidable');
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  ,fs = require('fs');
+var exec = require('child_process').exec;
 
-var formidable = require('formidable'),
-    form = new formidable.IncomingForm();
+
 
 var app = express();
 
@@ -32,6 +34,135 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+/*app.post('/upload',function (req,res) {
+  console.log('inside server');
+  console.log(req.data);
+  var form = new formidable.IncomingForm();
+  form.parse(req);
+  form.on('fileBegin', function (name, file){
+    file.path = __dirname + '/uploads/' + file.name;
+  });
+  form.on('file', function (name, file){
+    console.log('Uploaded ' + file.name);
+  });
+  form.uploadDir = __dirname + '/tmp';
+  form.encoding = 'binary';
+  console.log(form);
+  form.addListener('file', function(name, file) {
+    // do something with uploaded file
+  });
+
+  form.addListener('end', function() {
+    res.end();
+  });
+
+  form.parse(req, function(err, fields, files) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+});*/
+
+/*app.post('/upload', function(req, res){
+
+  console.log('inside server');
+
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+  console.log(req.params+"       "+form);
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('fileBegin', function (name, file){
+    file.path = __dirname + '/uploads/' + file.name;
+  });
+  form.on('file', function (name, file){
+    console.log('Uploaded ' + file.name);
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+
+});*/
+
+app.post('/upload', function(req, res){
+  console.log('inside server');
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+console.log(form);
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+console.log("1");
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+console.log("2"+form.uploadDir);
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    console.log(file);
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  console.log("1111"+file.name);
+    exec('java -jar "'+__dirname+ '\\sequence\\sequence-10.0.jar" --headless "'+__dirname+'\\uploads\\'+file.name+'"',
+        function (error, stdout, stderr){
+          if(stdout !== null) {
+            console.log("stdout -> "+stdout);
+            fs.readFile(__dirname+'\\uploads\\example.png',"binary",function (error,file) {
+              if(error)
+              {
+                res.setHeader("Content-Type", "text/plain" );
+                res.writeHead(500);
+                res.write(error + "\n");
+                res.end("error");
+              }
+              else
+              {
+                var img = fs.readFileSync(__dirname + "\\uploads\\example.png");
+                //console.log('going inside'+img);
+                //res.writeHead(200, {"Content-Type" : "image/png" });
+                //res.write(img, "binary" );
+                //res.end(img, "binary");
+                res.end(__dirname + "\\uploads\\example.png");
+              }
+            });
+          }
+          if(stderr !== null) {
+            console.log("stderr -> "+stderr.length+" "+stderr);
+          }
+          if(error !== null){
+            console.log("Error -> "+error);
+          }
+        });
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    // res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+
+});
 
 // app.post('/', function (req, res) {
 //   //var form = new formidable.IncomingForm();
--- public/angularjs/cloudapp.js
+++ public/angularjs/cloudapp.js
@@ -1,67 +1,178 @@
 var cloudapp=angular.module("CloudApp",[]);
 
-cloudapp.directive('fileModel', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
+// cloudapp.directive('fileModel', ['$parse', function ($parse) {
+//     return {
+//         restrict: 'A',
+//         link: function(scope, element, attrs) {
+//             var model = $parse(attrs.fileModel);
+//             var modelSetter = model.assign;
+//
+//             element.bind('change', function(){
+//                 scope.$apply(function(){
+//                     modelSetter(scope, element[0].files[0]);
+//                 });
+//             });
+//         }
+//     };
+// }]);
 
-            element.bind('change', function(){
-                scope.$apply(function(){
-                    modelSetter(scope, element[0].files[0]);
-                });
+// cloudapp.service('fileUpload', ['$http', function ($http) {
+//     this.uploadFileToUrl = function(file, uploadUrl){
+//         var fd = new FormData();
+//         fd.append('file', file);
+//
+//         $http.post(uploadUrl, fd, {
+//             transformRequest: angular.identity,
+//             headers: {'Content-Type': undefined}
+//         })
+//
+//             .success(function(){
+//             })
+//
+//             .error(function(){
+//             });
+//     }
+// }]);
+
+cloudapp.directive("fileinput", [function() {
+    return {
+        scope: {
+            fileinput: "=",
+            //filepreview: "="
+        },
+        link: function($scope, element, attributes) {
+            element.bind("change", function(changeEvent) {
+                $scope.fileinput = changeEvent.target.files[0];
+                var reader = new FileReader();
+                // reader.onload = function(loadEvent) {
+                //     $scope.$apply(function() {
+                //         //scope.filepreview = loadEvent.target.result;
+                //     });
+                // }
+                reader.readAsDataURL($scope.fileinput);
             });
         }
-    };
-}]);
-
-cloudapp.service('fileUpload', ['$http', function ($http) {
-    this.uploadFileToUrl = function(file, uploadUrl){
-        var fd = new FormData();
-        fd.append('file', file);
-
-        $http.post(uploadUrl, fd, {
-            transformRequest: angular.identity,
-            headers: {'Content-Type': undefined}
-        })
-
-            .success(function(){
-            })
-
-            .error(function(){
-            });
     }
 }]);
+cloudapp.controller("cloudCtrl", ['$scope', '$http', 'uploadService', function($scope, $http, uploadService) {
+    $scope.$watch('file', function(newfile, oldfile) {
+        if(angular.equals(newfile, oldfile) ){
+            return;
+        }
+        console.log("NEWFILE"+newfile);
+        /*var files = $scope.fileUpload.files;
+        console.log(files[0]);*/
+        //if (files.length > 0) {
+            // create a FormData object which will be sent as the data payload in the
+            // AJAX request
+            var formData = new FormData();
 
+            // loop through all the selected files and add them to the formData object
+           // for (var i = 0; i < files.length; i++) {
 
-cloudapp.controller('cloudCtrl', ['$scope', 'fileUpload', function($scope, fileUpload){
-    $scope.uploadFile = function(){
-        var file = $scope.myFile;
+                // add the files to formData object for the data payload
+                formData.append('uploads[]', newfile , newfile.name);
+        //formData.append(file.name,file);
+         //   }
+        //}
+        console.log("FORMDATA:"+formData.length);
+        uploadService.upload(newfile,formData).then(function(res){
+            // DO SOMETHING WITH THE RESULT!
+            console.log("result", res);
+        })
+    });
 
-        console.log('file is ' );
-        console.dir(file);
-
-        var uploadUrl = "/upload";
-        fileUpload.uploadFileToUrl(file, uploadUrl);
-    };
 }]);
 
 
+cloudapp.service("uploadService", function($http, $q,$window) {
 
+    return ({
+        upload: upload
+    });
 
-cloudapp.controller('cloudCtrl',function ($scope,$http) {
-$scope.uploadFile=function (files) {
-    var fd = new FormData();
-    //Take the first selected file
-    fd.append("file", files[0]);
-    console.log(fd.filename);
-    $http.post('/upload', fd, {
-        withCredentials: true,
-        headers: {'Content-Type': undefined },
-        transformRequest: angular.identity
-    }).success(console.log('success') ).error( console.log('error') );
+    function upload(file,formData) {
+        $window.alert('inside sercie');
+        console.log(file+" ======="+formData.length);
+        var upl = $http({
+            method: 'POST',
+            url: '/upload', // /api/upload,
+                processData: false,
+                contentType: false,
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined},
+            /* headers: {
+                 'Content-Type': 'multipart/form-data;'
+             },*/
+            data: {
+                upload:formData
+            }
+            // transformRequest: function(data, headersGetter) {
+            //     var formData = new FormData();
+            //     angular.forEach(data, function(value, key) {
+            //         formData.append(key, value);
+            //     });
+            //
+            //     var headers = headersGetter();
+            //     delete headers['Content-Type'];
+            //
+            //     return formData;
+            // }
+        }
+        );
+        $window.alert('inside sercie'+file+formData);
+        console.log(file+"========"+formData);
 
-}
-    
+        return upl.then(handleSuccess, handleError);
+
+    } // End upload function
+
+    // ---
+    // PRIVATE METHODS.
+    // ---
+
+    function handleError(response, data) {
+        if (!angular.isObject(response.data) ||!response.data.message) {
+            return ($q.reject("An unknown error occurred."));
+        }
+
+        return ($q.reject(response.data.message));
+    }
+
+    function handleSuccess(response) {
+        console.log(response);
+        return (response);
+    }
+
 });
+
+// cloudapp.controller('cloudCtrl', ['$scope', 'fileUpload', function($scope, fileUpload){
+//     $scope.uploadFile = function(){
+//         var file = $scope.myFile;
+//
+//         console.log('file is ' );
+//         console.dir(file);
+//
+//         var uploadUrl = "/upload";
+//         fileUpload.uploadFileToUrl(file, uploadUrl);
+//     };
+// }]);
+
+
+
+
+// cloudapp.controller('cloudCtrl',function ($scope,$http) {
+// $scope.uploadFile=function (files) {
+//     var fd = new FormData();
+//     //Take the first selected file
+//     fd.append("file", files[0]);
+//     console.log(fd.filename);
+//     $http.post('/upload', fd, {
+//         withCredentials: true,
+//         headers: {'Content-Type': undefined },
+//         transformRequest: angular.identity
+//     }).success(console.log('success') ).error( console.log('error') );
+//
+// }
+//
+// });
