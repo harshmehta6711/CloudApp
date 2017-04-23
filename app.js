7f21ab5c0f484ebe325542e8d5411b5652e9d7e8
harshmehta6711 <harsh.mehta@sjsu.edu>
2017-04-23 03:29:21 -0700

    Working code with multer module

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 /**
  * Module dependencies.
  */
+var connect=require('connect');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+
+
 
 var formidable = require('formidable');
 var express = require('express')
@@ -12,7 +17,7 @@ var express = require('express')
   ,fs = require('fs');
 var exec = require('child_process').exec;
 
-
+var multer=require('multer');
 
 var app = express();
 
@@ -20,174 +25,132 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+//app.use(express.methodOverride());
+app.use(bodyParser.urlencoded({extended: false}));
+//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+var storage = multer.diskStorage({
+  destination: function (request, file, callback) {
+    console.log(__dirname+'/uploads/');
+    callback(null, './uploads/');
+  },
+  filename: function (request, file, callback) {
+    console.log(file);
+    // callback(null, file.originalname)
+    callback(null, file.originalname+'-'+Date.now()+path.extname(file.originalname))
+  }
+});
+
+//var upload = multer({storage: storage});
+
+// var upload = multer({ dest: './uploads/'});
+// app.use(multer({ dest: './uploads/'}));
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  //app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-/*app.post('/upload',function (req,res) {
-  console.log('inside server');
-  console.log(req.data);
-  var form = new formidable.IncomingForm();
-  form.parse(req);
-  form.on('fileBegin', function (name, file){
-    file.path = __dirname + '/uploads/' + file.name;
-  });
-  form.on('file', function (name, file){
-    console.log('Uploaded ' + file.name);
-  });
-  form.uploadDir = __dirname + '/tmp';
-  form.encoding = 'binary';
-  console.log(form);
-  form.addListener('file', function(name, file) {
-    // do something with uploaded file
-  });
 
-  form.addListener('end', function() {
-    res.end();
-  });
 
-  form.parse(req, function(err, fields, files) {
-    if (err) {
-      console.log(err);
-    }
-  });
 
-});*/
 
-/*app.post('/upload', function(req, res){
-
-  console.log('inside server');
-
-  // create an incoming form object
-  var form = new formidable.IncomingForm();
-  console.log(req.params+"       "+form);
-  // store all uploads in the /uploads directory
-  form.uploadDir = path.join(__dirname, '/uploads');
-
-  // every time a file has been uploaded successfully,
-  // rename it to it's orignal name
-  form.on('fileBegin', function (name, file){
-    file.path = __dirname + '/uploads/' + file.name;
-  });
-  form.on('file', function (name, file){
-    console.log('Uploaded ' + file.name);
-  });
-
-  // log any errors that occur
-  form.on('error', function(err) {
-    console.log('An error has occured: \n' + err);
-  });
-
-  // once all the files have been uploaded, send a response to the client
-  form.on('end', function() {
-    res.end('success');
-  });
-
-  // parse the incoming request containing the form data
-  form.parse(req);
-
-});*/
-
-app.post('/upload', function(req, res){
-  console.log('inside server');
-  // create an incoming form object
-  var form = new formidable.IncomingForm();
-console.log(form);
-  // specify that we want to allow the user to upload multiple files in a single request
-  form.multiples = true;
-console.log("1");
-
-  // store all uploads in the /uploads directory
-  form.uploadDir = path.join(__dirname, '/uploads');
-console.log("2"+form.uploadDir);
-  // every time a file has been uploaded successfully,
-  // rename it to it's orignal name
-  form.on('file', function(field, file) {
-    console.log(file);
-    fs.rename(file.path, path.join(form.uploadDir, file.name));
-  console.log("1111"+file.name);
-    exec('java -jar "'+__dirname+ '\\sequence\\sequence-10.0.jar" --headless "'+__dirname+'\\uploads\\'+file.name+'"',
-        function (error, stdout, stderr){
-          if(stdout !== null) {
-            console.log("stdout -> "+stdout);
-            fs.readFile(__dirname+'\\uploads\\example.png',"binary",function (error,file) {
-              if(error)
-              {
-                res.setHeader("Content-Type", "text/plain" );
-                res.writeHead(500);
-                res.write(error + "\n");
-                res.end("error");
-              }
-              else
-              {
-                var img = fs.readFileSync(__dirname + "\\uploads\\example.png");
-                //console.log('going inside'+img);
-                //res.writeHead(200, {"Content-Type" : "image/png" });
-                //res.write(img, "binary" );
-                //res.end(img, "binary");
-                res.end(__dirname + "\\uploads\\example.png");
-              }
-            });
-          }
-          if(stderr !== null) {
-            console.log("stderr -> "+stderr.length+" "+stderr);
-          }
-          if(error !== null){
-            console.log("Error -> "+error);
-          }
-        });
-  });
-
-  // log any errors that occur
-  form.on('error', function(err) {
-    console.log('An error has occured: \n' + err);
-  });
-
-  // once all the files have been uploaded, send a response to the client
-  form.on('end', function() {
-    // res.end('success');
-  });
-
-  // parse the incoming request containing the form data
-  form.parse(req);
-
-});
-
-// app.post('/', function (req, res) {
-//   //var form = new formidable.IncomingForm();
+// app.post('/upload', function(req, res){
+//   console.log('inside server');
+//   // create an incoming form object
+//   var form = new formidable.IncomingForm();
+// console.log(form);
+//   // specify that we want to allow the user to upload multiple files in a single request
+//   form.multiples = true;
+// console.log("1");
 //
+//   // store all uploads in the /uploads directory
+//   form.uploadDir = path.join(__dirname, '/uploads');
+// console.log("2"+form.uploadDir);
+//   // every time a file has been uploaded successfully,
+//   // rename it to it's orignal name
+//   form.on('file', function(field, file) {
+//     console.log(file);
+//     fs.rename(file.path, path.join(form.uploadDir, file.name));
+//   console.log("1111"+file.name);
+//     exec('java -jar "'+__dirname+ '\\sequence\\sequence-10.0.jar" --headless "'+__dirname+'\\uploads\\'+file.name+'"',
+//         function (error, stdout, stderr){
+//           if(stdout !== null) {
+//             console.log("stdout -> "+stdout);
+//             fs.readFile(__dirname+'\\uploads\\example.png',"binary",function (error,file) {
+//               if(error)
+//               {
+//                 res.setHeader("Content-Type", "text/plain" );
+//                 res.writeHead(500);
+//                 res.write(error + "\n");
+//                 res.end("error");
+//               }
+//               else
+//               {
+//                 var img = fs.readFileSync(__dirname + "\\uploads\\example.png");
+//                 //console.log('going inside'+img);
+//                 //res.writeHead(200, {"Content-Type" : "image/png" });
+//                 //res.write(img, "binary" );
+//                 //res.end(img, "binary");
+//                 res.end(__dirname + "\\uploads\\example.png");
+//               }
+//             });
+//           }
+//           if(stderr !== null) {
+//             console.log("stderr -> "+stderr.length+" "+stderr);
+//           }
+//           if(error !== null){
+//             console.log("Error -> "+error);
+//           }
+//         });
+//   });
+//
+//   // log any errors that occur
+//   form.on('error', function(err) {
+//     console.log('An error has occured: \n' + err);
+//   });
+//
+//   // once all the files have been uploaded, send a response to the client
+//   form.on('end', function() {
+//     // res.end('success');
+//   });
+//
+//   // parse the incoming request containing the form data
 //   form.parse(req);
 //
-//   form.on('fileBegin', function (name, file) {
-//     file.path = __dirname + '/uploads/' + file.name;
-//   });
-//   form.on('file', function (name, file){
-//     console.log('Uploaded ' + file.name);
-//   });
-//
-//   res.sendFile(__dirname + '/index.html');
 // });
 
-// app.post('/upload', function(req, res, next) {
-//   form.parse(req, function(err, fields, files) {
-//     console.log("File received:\nName:"+files.pdf.name+"\ntype:"+files.pdf.type);
-//   });
-
-// app.post('/upload',function (req,res) {
-//   console.log("Hi Harsh"+req.data);
-//
+// app.post('/fileUpload', function(request, response) {
+//   upload(request, response, function(err) {
+//     if(err) {
+//       console.log('Error Occured');
+//       return;
+//     }
+//     console.log(request.body);
+//     response.end('Your File Uploaded');
+//     console.log('Photo Uploaded');
+//   })
 // });
+app.post('/fileUpload', function(req, res){
+  var upload = multer({
+    storage: storage
+  }).single('file')
+  upload(req, res, function(err) {
+    res.end('File is uploaded')
+  })
+  //console.log(req.body); // form fields
+  console.log('./uploads/');
+  console.log(req.files); // form files
+  res.status(204).end();
+});
 
 
 
